4d2309e69c7f67e3b1e3036c8c83de1c02fb7b63
MarlonVBP <marlonvicctor13@gmail.com>
2024-06-05 14:20:01 -0300

    Rotas_organizadas

--- routes/web.php
+++ routes/web.php
@@ -20,7 +20,7 @@ Route::post('/contato', [App\Http\Controllers\ContatoController::class, 'contato
 
 Route::get('/sobrenos', [App\Http\Controllers\SobreNosController::class, 'sobreNos'])->name('site.sobrenos');
 
-Route::get('/login', [App\Http\Controllers\SobreNosController::class, 'login'])->name('site.login');
+Route::get('/login', [App\Http\Controllers\LoginController::class, 'login'])->name('site.login');
 
 Route::prefix('app')->group(function(){
     Route::get('/fornecedores', [App\Http\Controllers\FornecedoresController::class, 'fornecedores'])->name('app.fornecedores');
